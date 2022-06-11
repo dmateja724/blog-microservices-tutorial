affa769e0975c6f3e19cc81c2643b0e1fe70b6fc
Derrick Mateja <dmateja724@gmail.com>
2022-06-11 16:14:36 -0500

    added event bus data store to handle missed events

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+const { reset } = require("nodemon");
 
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log(err.message);
@@ -25,6 +30,10 @@ app.post("/events", (req, res) => {
   });
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
